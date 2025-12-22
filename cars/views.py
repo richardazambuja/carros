cd252317baa63b213699f4c74b0282e1cd8ff76d
Richard Azzambuja <richardbarreeto@gmail.com>
2025-12-22 10:57:42 -0300

    testando a função render

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 def car_view(request):
-    return HttpResponse ('Meus carros')
+    return render (request, 'cars.html')
