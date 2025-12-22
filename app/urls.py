d96eb89269552252ef4e296584b9999c5b3aee43
Richard Azzambuja <richardbarreeto@gmail.com>
2025-12-22 09:43:38 -0300

    testando view com HttpResponse

--- app/urls.py
+++ app/urls.py
@@ -18,7 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+from cars.views import car_view
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cars/', car_view),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
