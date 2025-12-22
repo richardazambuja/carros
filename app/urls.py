bef7f2217f99606751883fdfd64dccc57c25f995
Richard Azzambuja <richardbarreeto@gmail.com>
2025-12-22 20:24:10 -0300

    retornando template no paremetro contexto do render

--- app/urls.py
+++ app/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import HttpResponse
 from cars.views import car_view
 
 
--- cars/views.py
+++ cars/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def car_view(request):
-    return render (request, 'cars.html')
+    return render (
+        request,
+        'cars.html',
+        {'cars': {'model': 'Fiat Uno 1.0', 'ano': '1996'}}
+    )
