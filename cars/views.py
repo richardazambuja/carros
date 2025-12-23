c73d1bc084514207cfaa09e3da3faae27edb86d6
Richard Azzambuja <richardbarreeto@gmail.com>
2025-12-23 09:54:11 -0300

    buscando carros no banco de dados com Django ORM

--- cars/views.py
+++ cars/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-
+from cars.models import Car
 
 # Create your views here.
 def car_view(request):
+    
+    cars = Car.objects.all()
+    
+
     return render (
         request,
         'cars.html',
-        {'cars': {'model': 'Fiat Uno 1.0', 'ano': '1996'}}
+        {'cars': cars}
     )
