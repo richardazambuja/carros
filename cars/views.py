c945c812fcb74805c798565e3a5255361bb4c5fa
Richard Azzambuja <richardbarreeto@gmail.com>
2025-12-23 14:14:17 -0300

    filtrando carros com parâmetros do usuário

--- cars/views.py
+++ cars/views.py
@@ -3,8 +3,11 @@ from cars.models import Car
 
 # Create your views here.
 def car_view(request):
-    
-    cars = Car.objects.filter(model__contains='Uno')
+    cars = Car.objects.all()
+    search = request.GET.get('search')
+
+    if search:
+        cars = cars.filter(model__contains=search)
     
 
     return render (
