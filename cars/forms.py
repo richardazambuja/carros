c53e09083510fd474f7d59cf34e33e461669f08c
Richard Azzambuja <richardbarreeto@gmail.com>
2025-12-26 20:03:01 -0300

    adicionando validações baseadas nas  regras do negócio

--- cars/forms.py
+++ cars/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from cars.models import Brand, Car
+from cars.models import Car, Brand
 
-
+#Formulário antigo
 class CarForm(forms.Form):
     model = forms.CharField(max_length=200)
     brand = forms.ModelChoiceField(Brand.objects.all())
@@ -25,7 +25,21 @@ class CarForm(forms.Form):
         car.save()
         return car
 
+
+#Formulário em uso
 class CarModelForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = '__all__'
+
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value < 20000:
+            self.add_error('value', "O valor minímo do carro deve ser de R$20.000,00")
+        return value
+    
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year < 1990:
+            self.add_error('factory_year', "Ano inválido")
+        return factory_year
