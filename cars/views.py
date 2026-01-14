d3d6f1b5fcfd0114efb173303ae1a19c38ac2c40
richardazambuja <richardbarreeto@gmail.com>
2026-01-14 13:09:50 -0300

    Utilizando CreateView e criando rota de sucesso pós cadastro

--- cars/views.py
+++ cars/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarForm, CarModelForm
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 
 ##FunctionBasedView
 """def car_view(request):
@@ -21,16 +21,6 @@ from django.views.generic import ListView
 
 
 ##Refatoração da FunctionBasedView no modelo ClassBasedView
-class CarView(View):
-
-    def get(self, request):
-        cars = Car.objects.all().order_by('model')
-        search = request.GET.get('search')
-
-        if search:
-            cars = cars.filter(model_icontains=search)
-
-        return render(request, 'cars.html', {'cars':cars})
 
 
 class CarListView(ListView):
@@ -51,8 +41,8 @@ class CarListView(ListView):
 
 
 
-
-class NewCarView(View):
+"""NewCarView herdando somente da classe View"""
+"""class NewCarView(View):
     def get(self, request):
         if request.user.is_authenticated:
             new_car_form = CarModelForm()
@@ -65,3 +55,16 @@ class NewCarView(View):
                 new_car_form.save()
                 return redirect('cars_list')
             return render(request, 'new_car.html', {'new_car_form': new_car_form})
+"""
+
+
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/cars/'
+
+class SucessView(View):
+    def get(self, request):
+        user = request.user 
+        return render(request, 'sucess.html', )
