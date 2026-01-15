d1f7097c38e96a931d88ef98146fbb693af976a3
richardazambuja <richardbarreeto@gmail.com>
2026-01-15 13:01:09 -0300

    Implementando DetailView e melhorando UI/UX

--- cars/views.py
+++ cars/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarForm, CarModelForm
 from django.views import View
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 ##FunctionBasedView
@@ -78,3 +78,9 @@ class SuccessView(View):
 class CarDetailView(DetailView):
      model = Car
      template_name = 'car_detail.html'
+
+class CarUpdateView(UpdateView):
+     model = Car
+     form_class = CarModelForm
+     template_name = 'car_update.html'
+     success_url = '/cars/'
