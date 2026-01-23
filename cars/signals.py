00dd1d70be0093448c9ff54b4958b459f6967972
Richard Azzambuja <richardbarreeto@gmail.com>
2026-01-23 20:59:54 -0300

    Testando Signals

---
+++ cars/signals.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import pre_save, pre_delete, post_save, post_delete
+from django.dispatch import receiver
+from cars.models import Car
+
+
+@receiver(pre_save, sender=Car)
+def car_pre_save(sender, instance, **kwargs):
+    print('### PRE SAVE ###')
+    print(instance)
+
+
+@receiver(post_save, sender=Car)
+def car_post_save(sender, instance, **kwargs):
+    print('### POST SAVE ###')
+    print(instance)
