d658c3cca774c69827a7723738b2a97a3feda55e
Richard Azzambuja <richardbarreeto@gmail.com>
2026-01-29 20:17:16 -0300

    Criando bio automática para os carros

--- cars/signals.py
+++ cars/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save, post_delete, pre_save
 from django.db.models import Sum
 from django.dispatch import receiver
 from cars.models import Car, Car_inventory
@@ -15,6 +15,11 @@ def car_inventory_update():
         cars_value=cars_value
     )
 
+
+@receiver(pre_save, sender=Car)
+def car_pre_save(sender, instance, **kwargs):
+    if not instance.bio:
+        instance.bio = 'Descrição gerada automáticamente'
 
 
 @receiver(post_save, sender=Car)
