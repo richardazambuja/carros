fc96a32d32101a03688c76521b40a3eb9e158a4c
Richard Azzambuja <richardbarreeto@gmail.com>
2026-02-03 22:25:14 -0300

    Migrando o banco de dados para PostgreSQL

--- cars/signals.py
+++ cars/signals.py
@@ -16,12 +16,6 @@ def car_inventory_update():
     )
 
 
-@receiver(pre_save, sender=Car)
-def car_pre_save(sender, instance, **kwargs):
-    if not instance.bio:
-        instance.bio = 'Descrição gerada automáticamente'
-
-
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
     car_inventory_update()
